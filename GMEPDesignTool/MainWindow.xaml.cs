7cd91771ea7bd91e38caa3e0c92a36331c20d250
grenrock <stevengrenrock@live.com>
2024-11-19 08:18:26 -0800

    initial commit

---
+++ GMEPDesignTool/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+
+namespace GMEPDesignTool
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  /// 
+  public partial class MainWindow
+  {
+    public ViewModel MainWindowViewModel { get; set; }
+    public MainWindow()
+    {
+      MainWindowViewModel = new ViewModel();
+      DataContext = MainWindowViewModel;
+      InitializeComponent();
+    }
+  }
+}
---
+++ GMEPDesignTool/ViewModel.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Input;
+
+namespace GMEPDesignTool
+{
+  public class ViewModel : ViewModelBase
+  {
+    private string _name = "";
+    public string Name
+    {
+      get => _name;
+      set => SetProperty(ref _name, value);
+    }
+
+    private List<Dictionary<string, string>> _searchResults = new List<Dictionary<string, string>>();
+    public List<Dictionary<string, string>> SearchResults
+    {
+      get => _searchResults;
+      set => SetProperty(ref _searchResults, value);
+    }
+
+    private List<string> _searchResultKeys = new List<string>();
+    public List<string> SearchResultKeys
+    {
+      get => _searchResultKeys;
+      set => SetProperty(ref _searchResultKeys, value);
+    }
+
+    private string _searchText = "";
+    public string SearchText
+    {
+      get => _searchText;
+      set
+      {
+        if (SetProperty(ref _searchText, value))
+        {
+          if (value.Length > 5)
+          {
+            GetSearchResultsCommand.Execute(this);
+          }
+        }
+      }
+    }
+
+    private readonly DelegateCommand _getSearchResultsCommand;
+    public ICommand GetSearchResultsCommand => _getSearchResultsCommand;
+
+    public ViewModel()
+    {
+      _getSearchResultsCommand = new DelegateCommand(GetSearchResults, CanGetSearchResults);
+      Name = "My Name";
+    }
+
+    private void GetSearchResults(object commandParameter)
+    {
+      SearchResults = new List<Dictionary<string, string>>();
+      SearchResultKeys = new List<string>();
+      Dictionary<string, string> thing = new Dictionary<string, string>();
+      thing["24-100"] = "Z:\\lskdjfksljf";
+      SearchResults.Add(thing);
+      SearchResultKeys.Add(SearchText);
+    }
+
+    private bool CanGetSearchResults(object commandParameter)
+    {
+      return true;
+    }
+  }
+}
