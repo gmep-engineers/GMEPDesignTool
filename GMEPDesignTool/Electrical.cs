03423d4cc0e59a7d89bed4cc98e354c58478e838
grenrock <stevengrenrock@live.com>
2024-11-22 08:17:33 -0800

    add user control to project tabs

--- GMEPDesignTool/Electrical.cs
+++ GMEPDesignTool/Electrical.cs
@@ -9,60 +9,60 @@ namespace GMEPDesignTool
 {
   public class Electrical
   {
-    private List<ElectricalService> serviceList;
-    private List<ElectricalPanel> panelList;
-    private List<ElectricalEquipment> equipmentList;
-    private DateTime LastSaved;
-    private List<string> sqlActions;
+    public List<ElectricalService> ServiceList;
+    public List<ElectricalPanel> PanelList;
+    public List<ElectricalEquipment> EquipmentList;
+    public DateTime LastSaved;
+    public List<string> sqlActions;
     public Electrical()
     {
-      serviceList = new List<ElectricalService>();
-      panelList = new List<ElectricalPanel>();
-      equipmentList = new List<ElectricalEquipment>();
+      ServiceList = new List<ElectricalService>();
+      PanelList = new List<ElectricalPanel>();
+      EquipmentList = new List<ElectricalEquipment>();
       sqlActions = new List<string>();
     }
     public void AddService(ElectricalService electricalService)
     {
       if (!electricalService.Verify()) { return; }
-      serviceList.Add(electricalService);
+      ServiceList.Add(electricalService);
       sqlActions.Add($"INSERT INTO electrical_services VALUES('{electricalService.Id}', '{electricalService.Name}', '{electricalService.ProjectId}')");
     }
     public void UpdateService(int serviceIndex, ElectricalService updatedElectricalService)
     {
       if (!updatedElectricalService.Verify()) { return; }
-      serviceList[serviceIndex] = updatedElectricalService;
+      ServiceList[serviceIndex] = updatedElectricalService;
     }
     public void RemoveService(int serviceIndex)
     {
-      serviceList.RemoveAt(serviceIndex);
+      ServiceList.RemoveAt(serviceIndex);
     }
     public void AddPanel(ElectricalPanel panel)
     {
       if (!panel.Verify()) { return; }
-      panelList.Add(panel);
+      PanelList.Add(panel);
     }
     public void UpdatePanel(int panelIndex, ElectricalPanel updatedPanel)
     {
       if (!updatedPanel.Verify()) { return; }
-      panelList[panelIndex] = updatedPanel;
+      PanelList[panelIndex] = updatedPanel;
     }
     public void RemovePanel(int panelIndex)
     {
-      panelList.RemoveAt(panelIndex);
+      PanelList.RemoveAt(panelIndex);
     }
     public void AddEquipment(ElectricalEquipment equipment)
     {
       if (!equipment.Verify()) { return; }
-      equipmentList.Add(equipment);
+      EquipmentList.Add(equipment);
     }
     public void UpdateEquipment(int equipmentIndex, ElectricalEquipment updatedEquipment)
     {
       if (!updatedEquipment.Verify()) { return; }
-      equipmentList[equipmentIndex] = updatedEquipment;
+      EquipmentList[equipmentIndex] = updatedEquipment;
     }
     public void RemoveEquipment(int equipmentIndex)
     {
-      equipmentList.RemoveAt(equipmentIndex);
+      EquipmentList.RemoveAt(equipmentIndex);
     }
 
     public void FlushActions()
