ec4cc0504722f6b0eceaa5abbc6d798798855afe
Ethan Stafford <ethanstafford.dev@gmail.com>
2024-11-22 13:04:53 -0800

    Adding/Removing Panels

--- GMEPDesignTool/ProjectControl.xaml.cs
+++ GMEPDesignTool/ProjectControl.xaml.cs
@@ -23,37 +23,63 @@ namespace GMEPDesignTool
   public partial class ProjectControl : UserControl
   {
 
-    public ObservableCollection<ElectricalPanel> ElectricalPanels { get; set; }
-    public ProjectControl()
-    {
-      InitializeComponent();
-      ElectricalPanels = new ObservableCollection<ElectricalPanel>();
-      this.DataContext = this;
+        public ObservableCollection<ElectricalPanel> ElectricalPanels { get; set; }
+        public ObservableCollection<ElectricalService> ElectricalServices { get; set; }
+        public ProjectControl()
+        {
+              InitializeComponent();
+              ElectricalPanels = new ObservableCollection<ElectricalPanel>();
+              ElectricalServices = new ObservableCollection<ElectricalService>();
+              this.DataContext = this;
         }
 
-    public void AddElectricalPanel(ElectricalPanel electricalPanel)
-    {
-      ElectricalPanels.Add(electricalPanel);
-    }
+        public void AddElectricalPanel(ElectricalPanel electricalPanel)
+        {
+          ElectricalPanels.Add(electricalPanel);
+        }
 
-    public void AddNewElectricalPanel(object sender, EventArgs e)
-    {
-      Trace.WriteLine("new panel");
-      ElectricalPanel electricalPanel = new ElectricalPanel("0", 100, 100, false, false, "", 0, "");
-      ElectricalPanels.Add(electricalPanel);
-      
-    }
+        public void AddNewElectricalPanel(object sender, EventArgs e)
+        {
+          Trace.WriteLine("new panel");
+          ElectricalPanel electricalPanel = new ElectricalPanel("0", 100, 100, false, false, "", 0, "");
+          ElectricalPanels.Add(electricalPanel);
+        }
 
-    public void RemoveElectricalPanel(ElectricalPanel electricalPanel)
-    {
-      ElectricalPanels.Remove(electricalPanel);
-    }
-    public void DeleteSelectedElectricalPanel(object sender, EventArgs e)
-    {
-        if (sender is Hyperlink hyperlink && hyperlink.CommandParameter is ElectricalPanel electricalPanel)
+        public void RemoveElectricalPanel(ElectricalPanel electricalPanel)
+        {
+          ElectricalPanels.Remove(electricalPanel);
+        }
+        public void DeleteSelectedElectricalPanel(object sender, EventArgs e)
+        {
+            if (sender is Hyperlink hyperlink && hyperlink.CommandParameter is ElectricalPanel electricalPanel)
+                {
+                    RemoveElectricalPanel(electricalPanel);
+                }
+        }
+
+        //Service Functions
+        public void AddElectricalService(ElectricalService electricalService)
+        {
+            ElectricalServices.Add(electricalService);
+        }
+
+        public void AddNewElectricalService(object sender, EventArgs e)
+        {
+            Trace.WriteLine("new service");
+            ElectricalService electricalService = new ElectricalService("0", "0", "", "", 0);
+            ElectricalServices.Add(electricalService);
+        }
+
+        public void RemoveElectricalService(ElectricalService electricalService)
+        {
+            ElectricalServices.Remove(electricalService);
+        }
+        public void DeleteSelectedElectricalService(object sender, EventArgs e)
+        {
+            if (sender is Hyperlink hyperlink && hyperlink.CommandParameter is ElectricalService electricalService)
             {
-                RemoveElectricalPanel(electricalPanel);
+                RemoveElectricalService(electricalService);
             }
-     }
+        }
     }
 }
