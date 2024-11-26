aad358f8e32310e7aa980e88a2b4f173cc08f916
Ethan Stafford <ethanstafford.dev@gmail.com>
2024-11-26 14:06:32 -0800

    Initial Database Setup

---
+++ GMEPDesignTool/Database/Database.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Xml.Linq;
+using MySql.Data.MySqlClient;
+
+namespace GMEPDesignTool.Database
+{
+    internal class Database
+    {
+        public string ConnectionString { get; set; }
+        public MySqlConnection Connection { get; set; }
+
+        public Database()
+        {
+            ConnectionString = Properties.Settings.Default.ConnectionString;
+            Connection = new MySqlConnection(ConnectionString);
+        }
+
+        public void OpenConnection()
+        {
+            if (Connection.State == System.Data.ConnectionState.Closed)
+            {
+                Connection.Open();
+            }
+        }
+
+        public void CloseConnection()
+        {
+            if (Connection.State == System.Data.ConnectionState.Open)
+            {
+                Connection.Close();
+            }
+        }
+
+        public void GetProjectPanels(string projectName)
+        {
+            string query =
+                "SELECT p.* FROM panels p JOIN projects pr ON p.projectId = pr.id WHERE pr.gmep_project_name = "
+                + projectName;
+        }
+    }
+}
