2bdf0cb37097d45647925399efc0daeb685038db
Ethan Stafford <ethanstafford.dev@gmail.com>
2024-12-11 15:52:37 -0800

    Initial Login-Window UI

---
+++ GMEPDesignTool/LoginViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace GMEPDesignTool
+{
+    public class LoginViewModel : ViewModelBase
+    {
+        private Window _loginWindow;
+
+        public LoginViewModel(Window loginWindow)
+        {
+            _loginWindow = loginWindow;
+        }
+
+        public void openApp()
+        {
+            Application.Current.Dispatcher.Invoke(() =>
+            {
+                MainWindow mainWindow = new MainWindow();
+                mainWindow.Show();
+                _loginWindow.Close();
+            });
+        }
+    }
+}
