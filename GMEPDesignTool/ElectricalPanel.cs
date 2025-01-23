dc13c487dbeef468097afd865fdd2d1c48afe1b9
Ethan Stafford <ethanstafford.dev@gmail.com>
2025-01-23 11:48:00 -0800

    meow

--- GMEPDesignTool/ElectricalPanel.cs
+++ GMEPDesignTool/ElectricalPanel.cs
@@ -220,34 +220,7 @@ namespace GMEPDesignTool
             }
         }
 
-        public string ColorCode
-        {
-            get => _colorCode;
-            set
-            {
-                _colorCode = value;
-                OnPropertyChanged();
-            }
-        }
-
-        public string FedFromId
-        {
-            get => _fedFromId;
-            set
-            {
-                _fedFromId = value;
-                OnPropertyChanged();
-            }
-        }
-        public string ProjectId
-        {
-            get => _projectId;
-            set
-            {
-                _projectId = value;
-                OnPropertyChanged();
-            }
-        }
+        
 
         public int NumBreakers
         {
