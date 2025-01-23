e4b19397daabf55c93627cfcdab98501bbdb5f08
Ethan Stafford <ethanstafford.dev@gmail.com>
2025-01-23 13:06:25 -0800

    Getnames issue fix

--- GMEPDesignTool/ProjectControl.xaml.cs
+++ GMEPDesignTool/ProjectControl.xaml.cs
@@ -38,8 +38,8 @@ namespace GMEPDesignTool
         public ObservableCollection<ElectricalEquipment> ElectricalEquipments { get; set; }
         public ObservableCollection<ElectricalLighting> ElectricalLightings { get; set; }
         public ObservableCollection<ElectricalTransformer> ElectricalTransformers { get; set; }
-        public ObservableCollection<KeyValuePair<string, string>> FedFromNames { get; set; }
-        public ObservableCollection<KeyValuePair<string, string>> PanelNames { get; set; }
+        public ObservableDictionary<string, string> FedFromNames { get; set; }
+        public ObservableDictionary<string, string> PanelNames { get; set; }
 
         public ObservableCollection<string> ImagePaths { get; set; }
         public Dictionary<string, string> Owners { get; set; }
@@ -61,8 +61,8 @@ namespace GMEPDesignTool
             ElectricalEquipments = database.GetProjectEquipment(ProjectId);
             ElectricalTransformers = database.GetProjectTransformers(ProjectId);
             ElectricalLightings = database.GetProjectLighting(ProjectId);
-            FedFromNames = new ObservableCollection<KeyValuePair<string, string>>();
-            PanelNames = new ObservableCollection<KeyValuePair<string, string>>();
+            FedFromNames = new ObservableDictionary<string, string>();
+            PanelNames = new ObservableDictionary<string, string>();
             Owners = database.getOwners();
             EquipmentViewSource = (CollectionViewSource)FindResource("EquipmentViewSource");
             EquipmentViewSource.Filter += EquipmentViewSource_Filter;
@@ -795,97 +795,112 @@ namespace GMEPDesignTool
 
         public void GetNames()
         {
-            Dictionary<string, string> fedFromBackup = new Dictionary<string, string>();
-            foreach (var panel in ElectricalPanels)
-            {
-                fedFromBackup[panel.Id] = panel.FedFromId;
-            }
 
-            Dictionary<string, string> panelBackup = new Dictionary<string, string>();
-            foreach (var equipment in ElectricalEquipments)
-            {
-                panelBackup[equipment.Id] = equipment.ParentId;
-            }
-            Dictionary<string, string> lightingBackup = new Dictionary<string, string>();
-            foreach (var lighting in ElectricalLightings)
-            {
-                lightingBackup[lighting.Id] = lighting.ParentId;
-            }
-
-            Dictionary<string, string> transformerBackup = new Dictionary<string, string>();
-            foreach (var transformer in ElectricalTransformers)
-            {
-                transformerBackup[transformer.Id] = transformer.ParentId;
-            }
-            FedFromNames.Clear();
-            PanelNames.Clear();
-            PanelNames.Add(new KeyValuePair<string, string>("", ""));
-            FedFromNames.Add(new KeyValuePair<string, string>("", ""));
             foreach (ElectricalService service in ElectricalServices)
             {
-                if (service.Name != "")
-                {
-                    KeyValuePair<string, string> value = new KeyValuePair<string, string>(
-                        service.Id,
-                        service.Name
-                    );
-                    FedFromNames.Add(value);
-                }
+
+                KeyValuePair<string, string> value = new KeyValuePair<string, string>(
+                    service.Id,
+                    service.Name
+                );
+                AddToFedFromNames(value);
             }
             foreach (ElectricalPanel panel in ElectricalPanels)
             {
-                if (panel.Name != "")
-                {
-                    KeyValuePair<string, string> value = new KeyValuePair<string, string>(
-                        panel.Id,
-                        panel.Name
-                    );
-                    FedFromNames.Add(value);
-                    PanelNames.Add(value);
-                }
+                KeyValuePair<string, string> value = new KeyValuePair<string, string>(
+                    panel.Id,
+                    panel.Name
+                );
+                AddToFedFromNames(value);
+                AddToPanelNames(value);
             }
             foreach (ElectricalTransformer transformer in ElectricalTransformers)
             {
-                if (transformer.Name != "")
+                KeyValuePair<string, string> value = new KeyValuePair<string, string>(
+                    transformer.Id,
+                    transformer.Name
+                );
+                AddToFedFromNames(value);
+                AddToPanelNames(value);
+
+            }
+
+            void AddToPanelNames(KeyValuePair<string, string> value)
+            {
+                if (PanelNames.ContainsKey(value.Key))
                 {
-                    KeyValuePair<string, string> value = new KeyValuePair<string, string>(
-                        transformer.Id,
-                        transformer.Name
-                    );
-                    FedFromNames.Add(value);
-                    PanelNames.Add(value);
+                    if (PanelNames[value.Key] != value.Value)
+                    {
+                        if (!string.IsNullOrEmpty(value.Value))
+                        {
+                            PanelNames[value.Key] =  value.Value;
+                        }
+                        else
+                        {
+                            PanelNames.Remove(value.Key);
+                        }
+                    }
+                }
+                else
+                {
+                    if (!string.IsNullOrEmpty(value.Value))
+                    {
+                        PanelNames.Add(value.Key, value.Value);
+                    }
                 }
             }
-            //adding backup values
-            foreach (var equipment in ElectricalEquipments)
+            void AddToFedFromNames(KeyValuePair<string, string> value)
             {
-                if (panelBackup.ContainsKey(equipment.Id))
+                if (FedFromNames.ContainsKey(value.Key))
                 {
-                    equipment.ParentId = panelBackup[equipment.Id];
+                    if (FedFromNames[value.Key] != value.Value)
+                    {
+                        if (!string.IsNullOrEmpty(value.Value))
+                        {
+                            FedFromNames[value.Key] = value.Value;
+                        }
+                        else
+                        {
+                            FedFromNames.Remove(value.Key);
+                        }
+                    }
+                }
+                else
+                {
+                    if (!string.IsNullOrEmpty(value.Value))
+                    {
+                        FedFromNames.Add(value.Key, value.Value);
+                    }
                 }
             }
-            foreach (var lighting in ElectricalLightings)
+            void CleanUpNames()
             {
-                if (lightingBackup.ContainsKey(lighting.Id))
+                var validIds = new HashSet<string>(
+                    ElectricalServices.Select(es => es.Id)
+                    .Concat(ElectricalTransformers.Select(et => et.Id))
+                    .Concat(ElectricalPanels.Select(ep => ep.Id))
+                );
+
+                var fedFromNamesKeys = FedFromNames.Keys.ToList();
+                foreach (var key in fedFromNamesKeys)
                 {
-                    lighting.ParentId = lightingBackup[lighting.Id];
+                    if (!validIds.Contains(key))
+                    {
+                        FedFromNames.Remove(key);
+                    }
                 }
-            }
-            foreach (var panel in ElectricalPanels)
-            {
-                if (fedFromBackup.ContainsKey(panel.Id))
+
+                var panelNamesKeys = PanelNames.Keys.ToList();
+                foreach (var key in panelNamesKeys)
                 {
-                    panel.FedFromId = fedFromBackup[panel.Id];
-                }
-            }
-            foreach (var transformer in ElectricalTransformers)
-            {
-                if (transformerBackup.ContainsKey(transformer.Id))
-                {
-                    transformer.ParentId = transformerBackup[transformer.Id];
+                    if (!validIds.Contains(key))
+                    {
+                        PanelNames.Remove(key);
+                    }
                 }
             }
         }
+
 
         private void ElectricalPanel_PropertyChanged(object? sender, PropertyChangedEventArgs e)
         {
@@ -1651,4 +1666,198 @@ namespace GMEPDesignTool
             return value;
         }
     }
+
+    [Serializable]
+    public class ObservableKeyValuePair<TKey, TValue> : INotifyPropertyChanged
+    {
+        #region properties
+        private TKey key;
+        private TValue value;
+
+        public TKey Key
+        {
+            get { return key; }
+            set
+            {
+                key = value;
+                OnPropertyChanged("Key");
+            }
+        }
+
+        public TValue Value
+        {
+            get { return value; }
+            set
+            {
+                this.value = value;
+                OnPropertyChanged("Value");
+            }
+        }
+        #endregion
+
+        #region INotifyPropertyChanged Members
+
+        [field: NonSerialized]
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string name)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(name));
+        }
+
+        #endregion
+    }
+
+    [Serializable]
+    public class ObservableDictionary<TKey, TValue> : ObservableCollection<ObservableKeyValuePair<TKey, TValue>>, IDictionary<TKey, TValue>
+    {
+
+        #region IDictionary<TKey,TValue> Members
+
+        public void Add(TKey key, TValue value)
+        {
+            if (ContainsKey(key))
+            {
+                throw new ArgumentException("The dictionary already contains the key");
+            }
+            base.Add(new ObservableKeyValuePair<TKey, TValue>() { Key = key, Value = value });
+        }
+
+        public bool ContainsKey(TKey key)
+        {
+            //var m=base.FirstOrDefault((i) => i.Key == key);
+            var r = ThisAsCollection().FirstOrDefault((i) => Equals(key, i.Key));
+
+            return !Equals(default(ObservableKeyValuePair<TKey, TValue>), r);
+        }
+
+        bool Equals<TKey>(TKey a, TKey b)
+        {
+            return EqualityComparer<TKey>.Default.Equals(a, b);
+        }
+
+        private ObservableCollection<ObservableKeyValuePair<TKey, TValue>> ThisAsCollection()
+        {
+            return this;
+        }
+
+        public ICollection<TKey> Keys
+        {
+            get { return (from i in ThisAsCollection() select i.Key).ToList(); }
+        }
+
+        public bool Remove(TKey key)
+        {
+            var remove = ThisAsCollection().Where(pair => Equals(key, pair.Key)).ToList();
+            foreach (var pair in remove)
+            {
+                ThisAsCollection().Remove(pair);
+            }
+            return remove.Count > 0;
+        }
+
+        public bool TryGetValue(TKey key, out TValue value)
+        {
+            value = default(TValue);
+            var r = GetKvpByTheKey(key);
+            if (Equals(r, default(ObservableKeyValuePair<TKey, TValue>)))
+            {
+                return false;
+            }
+            value = r.Value;
+            return true;
+        }
+
+        private ObservableKeyValuePair<TKey, TValue> GetKvpByTheKey(TKey key)
+        {
+            return ThisAsCollection().FirstOrDefault((i) => i.Key.Equals(key));
+        }
+
+        public ICollection<TValue> Values
+        {
+            get { return (from i in ThisAsCollection() select i.Value).ToList(); }
+        }
+
+        public TValue this[TKey key]
+        {
+            get
+            {
+                TValue result;
+                if (!TryGetValue(key, out result))
+                {
+                    throw new ArgumentException("Key not found");
+                }
+                return result;
+            }
+            set
+            {
+                if (ContainsKey(key))
+                {
+                    GetKvpByTheKey(key).Value = value;
+                }
+                else
+                {
+                    Add(key, value);
+                }
+            }
+        }
+
+        #endregion
+
+        #region ICollection<KeyValuePair<TKey,TValue>> Members
+
+        public void Add(KeyValuePair<TKey, TValue> item)
+        {
+            Add(item.Key, item.Value);
+        }
+
+        public bool Contains(KeyValuePair<TKey, TValue> item)
+        {
+            var r = GetKvpByTheKey(item.Key);
+            if (Equals(r, default(ObservableKeyValuePair<TKey, TValue>)))
+            {
+                return false;
+            }
+            return Equals(r.Value, item.Value);
+        }
+
+        public void CopyTo(KeyValuePair<TKey, TValue>[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsReadOnly
+        {
+            get { return false; }
+        }
+
+        public bool Remove(KeyValuePair<TKey, TValue> item)
+        {
+            var r = GetKvpByTheKey(item.Key);
+            if (Equals(r, default(ObservableKeyValuePair<TKey, TValue>)))
+            {
+                return false;
+            }
+            if (!Equals(r.Value, item.Value))
+            {
+                return false;
+            }
+            return ThisAsCollection().Remove(r);
+        }
+
+        #endregion
+
+        #region IEnumerable<KeyValuePair<TKey,TValue>> Members
+
+        public new IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator()
+        {
+            return (from i in ThisAsCollection() select new KeyValuePair<TKey, TValue>(i.Key, i.Value)).ToList().GetEnumerator();
+        }
+
+        #endregion
+    }
+
+
+
 }
