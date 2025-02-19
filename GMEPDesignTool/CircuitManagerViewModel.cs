7df3816b719f1c2b810aaca78eaa81d44c4dab88
Ethan Stafford <ethanstafford.dev@gmail.com>
2025-02-19 14:39:02 -0800

    Fixed index error

--- GMEPDesignTool/CircuitManagerViewModel.cs
+++ GMEPDesignTool/CircuitManagerViewModel.cs
@@ -486,10 +486,11 @@ namespace GMEPDesignTool
                 int sourceIndex = sourceCollection.IndexOf(sourceItem);
                 int targetIndex = targetItem != null ? targetCollection.IndexOf(targetItem) : targetCollection.Count;
 
-                if (targetIndex > targetCollection.Count - 1)
+                if (targetIndex > targetCollection.Count - 1 && targetIndex != 0)
                 {
                     targetIndex = targetCollection.Count - 1;
                 }
+                
 
                 if (sourceIndex != -1)
                 {
