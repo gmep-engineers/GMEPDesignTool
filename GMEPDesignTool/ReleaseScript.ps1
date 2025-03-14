783528a0620e9b35b25b0980fcb16fe67e02023a
grenrock <stevengrenrock@live.com>
2025-03-14 11:01:22 -0700

    use safe reader; add icon to release

--- GMEPDesignTool/ReleaseScript.ps1
+++ GMEPDesignTool/ReleaseScript.ps1
@@ -1,2 +1,2 @@
-﻿Compress-Archive -Path "bin", "Symbols", "Install.ps1" -DestinationPath "GMEPDesignTool.zip"
+﻿Compress-Archive -Path "bin", "Symbols", "Install.ps1", "gmep.ico" -DestinationPath "GMEPDesignTool.zip"
 Copy-Item "GMEPDesignTool.zip" -Destination "Z:\GMEP Engineers\Users\GMEP Softwares"
