c86938287211656f4e939489b3d4b8264daab3e1
Fiona Shang <FionaS@gmepe.com>
2025-07-10 09:51:51 -0700

    deleted proposal question

--- GMEPDesignTool/Proposal.cs
+++ GMEPDesignTool/Proposal.cs
@@ -31,36 +31,4 @@ namespace GMEPDesignTool
             EmployeeUsername = employeeUsername;
         }
     }
-
-    public class ProposalQuestion
-    {
-        public int Id;
-        public string Question;
-        public string QuestionType;
-        public string VariableName;
-
-        public ProposalQuestion(int id, string question, string questionType, string variableName)
-        {
-            Id = id;
-            Question = question;
-            QuestionType = questionType;
-            VariableName = variableName;
-        }
-    }
-
-    public class ProposalAnswer
-    {
-        public string Id;
-        public string ProjectId;
-        public int QuestionId;
-        public string Answer;
-
-        public ProposalAnswer(string id, string projectId, int questionId, string answer)
-        {
-            Id = id;
-            ProjectId = projectId;
-            QuestionId = questionId;
-            Answer = answer;
-        }
-    }
 }
